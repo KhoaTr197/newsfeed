4bdd2c72cfbfa90073e141d284e81e1404f855c5
KhoaTr197 <darkreaper197@gmail.com>
2025-04-11 21:36:30 +0700

    [Fix] Update real database files and adjust some compose config:
    - Add 2 new SQL script files init, data.
    Q: Why they have order number before their file name?
    A: To tell the Docker which file to run first: init the db structure -> add data to db
    
    - Remove some unnecessary config in `compose.yaml`.
    - Add some comments in `compose.yaml`.
    Q: Why?
    A: Because this is OUR project, my cuh. I ain't let you drown in the sea of confusion!

---
+++ server/db/01_init.sql
@@ -0,0 +1,36 @@
+CREATE TABLE `articles` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `title` nvarchar(100),
+  `content` text,
+  `thumbnail` text,
+  `publishedDate` datetime,
+  `userId` int,
+  `cateId` int,
+  `status` bool DEFAULT 1
+);
+
+CREATE TABLE `categories` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `cateName` nvarchar(50),
+  `status` bool DEFAULT 1
+);
+
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(50),
+  `password` varchar(32),
+  `email` varchar(100) UNIQUE,
+  `role` bit,
+  `status` bool DEFAULT 1
+);
+
+CREATE TABLE `contact` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` text,
+  `email` text,
+  `message` text
+);
+
+ALTER TABLE `articles` ADD FOREIGN KEY (`cateId`) REFERENCES `categories` (`id`);
+
+ALTER TABLE `articles`  ADD FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
