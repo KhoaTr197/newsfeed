ff510a1dc58c1d20df8322ca6ddf4328644ed821
KhoaTr197 <darkreaper197@gmail.com>
2025-04-11 11:25:43 +0700

    [Fix]: resolve Docker error
    - Updated `compose.yaml` to address misconfiguration issues.
    - Adjusted `.env.dev` for proper environment variable handling.
    - Fixed related issues in `database.js` for smoother db operation.
    - Add more codes in `index.js` to test the Docker Container.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const logger = require("./server/middlewares/logger")
+const routes = require("./server/routes")
 require('dotenv').config({
   path: "config/.env.dev"
 });
-// --------------------------------------
-const { HOST_NAME = "localhost", PORT = 8000 } = process.env;
+
+const config = {
+  server: {
+    host: process.env.HOST_NAME || "localhost",
+    port: process.env.PORT || 8000
+  }
+};
 
 const app = express()
 const log = logger()
@@ -13,12 +19,22 @@ app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
+app.get("/me", (req, res) => {
+  res.send("Hello Me!")
+})
+
+// Apply routes
+app.use(routes);
+
 // START - TEST
 const services = require("./server/services/test")
-
 services.getData()
 // END - TEST
 
-app.listen(PORT, () => {
-  log.master(`Server is running on http://${HOST_NAME}:${PORT}`)
+app.get('*', (req, res) => {
+  res.status(404).send();
+});
+
+app.listen(config.server.port, () => {
+  log.master(`Server is running on http://${config.server.host}:${config.server.port}`)
 })
--- server/db/database.js
+++ server/db/database.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 // --------------------------------------
 
 const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST || 'db',
+  user: process.env.DB_USER || 'root',
+  database: process.env.DB_NAME || 'newsfeed',
+  password: process.env.DB_PASSWORD || 'root',
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
